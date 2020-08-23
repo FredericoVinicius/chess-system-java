d5e1c177deb1b56365fad1f576ee9e443c501740
Frederico Vinicius <fredvsleite@gmail.com>
2020-08-23 08:20:58 -0400

    chess layer and print the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,22 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		super();
+		this.board = board;
+		position = null; //a posição da peça recém-criada é nula, não está colocada no tabuleiro board
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+	
+	
 
 }
